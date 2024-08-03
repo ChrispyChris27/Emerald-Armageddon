dbfcbf0e290d2ad741cf1afcbf33ab3d49396651
ChrispyChris27 <ctoborg@gmail.com>
2024-08-03 00:48:31 -0400

    increased berry yields

--- include/global.berry.h
+++ include/global.berry.h
@@ -12,9 +12,9 @@ struct Berry
     u16 size:10;
     u16 weedsBonus:3;
     u16 pestsBonus:3;
-    u8 maxYield;
-    u8 minYield:4;
-    u8 waterBonus:4;
+    u16 maxYield:6;
+    u16 minYield:6;
+    u16 waterBonus:4;
     const u8 *description1;
     const u8 *description2;
     u8 growthDuration;
@@ -78,14 +78,14 @@ struct BerryTree
     u8 stopGrowth:1;
     u16 minutesUntilNextStage:14;
     u16 mutationA:2;
-    u8 berryYield:5;
-    u8 pests:1;
+    u8 berryYield:6;
     u8 mutationB:2;
     u8 regrowthCount:4;
     u8 watered:4; // Used to keep track of bonuses in case of gradient watering
     u16 moistureLevel:7;
     u16 moistureClock:7;
-    u16 padding:2;
+    u16 pests:1;
+    u16 padding:1;
 };
 
 #endif // GUARD_GLOBAL_BERRY_H
--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -3299,7 +3299,7 @@ static u32 AI_CalcMoveEffectScore(u32 battlerAtk, u32 battlerDef, u32 move)
         if (AI_THINKING_STRUCT->aiFlags[battlerAtk] & AI_FLAG_WILL_SUICIDE && gBattleMons[battlerDef].statStages[STAT_EVASION] < 7)
         {
             if (aiData->hpPercents[battlerAtk] < 50 && AI_RandLessThan(128))
-                ADJUST_SCORE(DECENT_EFFECT);
+                ADJUST_SCORE(+10);
             else if (aiData->hpPercents[battlerAtk] < 30 && AI_RandLessThan(192))
                 ADJUST_SCORE(+10);
             else if (aiData->hpPercents[battlerAtk] < 10 && AI_RandLessThan(230))
