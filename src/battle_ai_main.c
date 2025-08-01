8318cfd177679ac1562d6e8f6c3e30e172ac10f3
ChrispyChris27 <ctoborg@gmail.com>
2025-07-31 23:50:11 -0400

    trainers on route 110

--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -1818,9 +1818,7 @@ static s32 AI_CheckBadMove(u32 battlerAtk, u32 battlerDef, u32 move, s32 score)
             break;
         case EFFECT_TEATIME:
             if (PartnerHasSameMoveEffectWithoutTarget(BATTLE_PARTNER(battlerAtk), move, aiData->partnerMove))
-            {
                 ADJUST_SCORE(-10);    
-            }
             break;   
         case EFFECT_FORESIGHT:
             if (gBattleMons[battlerDef].status2 & STATUS2_FORESIGHT)
