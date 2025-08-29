c32d137f806cb67caff9be5c46f48db7edb76e32
ChrispyChris27 <ctoborg@gmail.com>
2025-08-28 21:39:55 -0400

    fixed music

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -27130,7 +27130,7 @@ F_TRAINER_FEMALE |
             .species = SPECIES_GEODUDE,
             .gender = TRAINER_MON_RANDOM_GENDER,
             .iv = TRAINER_PARTY_IVS(0, 0, 0, 0, 0, 0),
-            .lvl = 22,
+            .lvl = 21,
             .nature = NATURE_HARDY,
             .dynamaxLevel = MAX_DYNAMAX_LEVEL,
             },
