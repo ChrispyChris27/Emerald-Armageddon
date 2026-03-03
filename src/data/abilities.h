38180b4587ee253ff40a9c832c62d5eb3bcafd24
ChrispyChris27 <ctoborg@gmail.com>
2026-03-02 23:52:56 -0500

    fixed text on abilities

--- src/data/abilities.h
+++ src/data/abilities.h
@@ -309,7 +309,7 @@ const struct AbilityInfo gAbilitiesInfo[ABILITIES_COUNT] =
     [ABILITY_MAGMA_ARMOR] =
     {
         .name = _("Magma Armor"),
-        .description = COMPOUND_STRING("Prevents freeze and crits."),
+        .description = COMPOUND_STRING("Stops freeze and crits."),
         .aiRating = 1,
         .breakable = TRUE,
     },
@@ -763,7 +763,7 @@ const struct AbilityInfo gAbilitiesInfo[ABILITIES_COUNT] =
     [ABILITY_LEAF_GUARD] =
     {
         .name = _("Leaf Guard"),
-        .description = COMPOUND_STRING("Blocks status in sun and crits."),
+        .description = COMPOUND_STRING("Blocks crits and status."),
         .aiRating = 2,
         .breakable = TRUE,
     },
