294500d1226cf7291289c1fb3000ad8981255a9a
ChrispyChris27 <ctoborg@gmail.com>
2026-03-21 12:49:22 -0400

    illuminate boosts accuracy

--- src/battle_util.c
+++ src/battle_util.c
@@ -10468,6 +10468,7 @@ u32 GetTotalAccuracy(enum BattlerId battlerAtk, enum BattlerId battlerDef, enum 
     switch (atkAbility)
     {
     case ABILITY_COMPOUND_EYES:
+    case ABILITY_ILLUMINATE:
         calc = (calc * 130) / 100; // 1.3 compound eyes boost
         break;
     case ABILITY_VICTORY_STAR:
