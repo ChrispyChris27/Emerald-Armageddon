7bd25087f6f36987583d909015cf4b231b92e358
ChrispyChris27 <ctoborg@gmail.com>
2026-09-23 22:01:15 -0400

    more victory road trainer teams

--- src/data/moves_info.h
+++ src/data/moves_info.h
@@ -21365,6 +21365,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
         .target = TARGET_SELECTED,
         .priority = 0,
         .category = DAMAGE_CATEGORY_SPECIAL,
+        .pulseMove = TRUE,
         .metronomeBanned = TRUE,
         .additionalEffects = ADDITIONAL_EFFECTS({
             .moveEffect = MOVE_EFFECT_STAT_MINUS,
